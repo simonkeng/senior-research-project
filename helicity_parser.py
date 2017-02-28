d44ebcdb069c839f041e07cc24d78b20fe005ecd
Simon Keng <simonkeng7@gmail.com>
2017-02-27 23:45:17 -0800

    built a string with total Gly in each protein with total AA in protein in order to calculate percent AA in proteins, exported as CSV, big step forward, exciting night.

--- helicity_parser.py
+++ helicity_parser.py
@@ -8,7 +8,7 @@ protein_sequence = ''
 
 
 FILE_INPUT = 'BetaBarrelTrimericSeq.txt'
-FILE_OUTPUT = 'BetaBarrelTrimericSeqHelicityOutput.txt'
+FILE_OUTPUT = 'BBarrelTriOUTTESTER.csv'
 sequence_file = open(FILE_INPUT)
 output_file = open(FILE_OUTPUT, 'w')
 
@@ -74,6 +74,12 @@ def get_segments(line):
     return segment
 
 
+# output_file.write('protein' + '\t' + 'start' + '\t' + 'end' + '\t' + 'segment' + '\t' + 'length' + '\t' + 'Gly' + '\t' + 'Ala' + '\t' + 'Cys' + '\t' + 'Trp' + '\t' + 'Tyr' + '\t' + 'Pro' + '\t' + 'Thr' + '\t' + 'Ser' + '\t' + 'Asn' + '\t' + 'Gln' + '\t' + 'Asp' + '\t' + 'Glu' + '\t' + 'His' + '\t' + 'Lys' + '\t' + 'Arg' + '\t' + 'Met' + '\t' + 'Phe' + '\t' + 'Leu' + '\t' + 'Val' + '\t' + 'Ile' + '\n')
+
+
+# print 'Protein' + 'Gly' + 'Ala' + 'Cys'
+
+
 for line in sequence_file:
     if line.startswith('>'):
         protein_name = get_protein_name(line)
@@ -85,6 +91,12 @@ for line in sequence_file:
         protein_names_and_sequences[protein_name] = sequence
 
 for key in protein_names_and_segments.keys():
+
+    l = []
+    t = 0
+
+    l.append(key)
+
     for segment in protein_names_and_segments.get(key):
 
         segment_sequence = protein_names_and_sequences.get(key)[segment[0] - 1:segment[1]]
@@ -110,8 +122,27 @@ for key in protein_names_and_segments.keys():
         Val = segment_sequence.count('V')
         Ile = segment_sequence.count('I')
 
+        t += len(segment_sequence)
 
-        output_file.write(key + '\t' + str(segment[0]) + '\t' + str(segment[1]) + '\t' + segment_sequence + '\t' + str(len(segment_sequence)) + '\t' + 'Gly' + '\t' + str(Gly) + '\t' + 'Ala' + '\t' + str(Ala) + '\t' + 'Cys' + '\t' + str(Cys) + '\t' + 'Trp' + '\t' + str(Trp) + '\t' + 'Tyr' + '\t' + str(Tyr) + '\t' + 'Pro' + '\t' + str(Pro) + '\t' + 'Thr' + '\t' + str(Thr) + '\t' + 'Ser' + '\t' + str(Ser) + '\t' + 'Asn' + '\t' + str(Asn) + '\t' + 'Gln' + '\t' + str(Gln) + '\t' + 'Asp' + '\t' + str(Asp) + '\t' + 'Glu' + '\t' + str(Glu) + '\t' + 'His' + '\t' + str(His) + '\t' + 'Lys' + '\t' + str(Lys) + '\t' + 'Arg' + '\t' + str(Arg) + '\t' + 'Met' + '\t' + str(Met) + '\t' + 'Phe' + '\t' + str(Phe) + '\t' + 'Leu' + '\t' + str(Leu) + '\t' + 'Val' + '\t' + str(Val) + '\t' + 'Ile' + '\t' + str(Ile) + '\n')
+
+        l.append(Ala)
+
+
+    a = sum(l[1:])
+
+    l.insert(1, a)
+    l.insert(2, t)
+    print ', '.join(str(i) for i in l[0:3])
+
+
+    # output_file.write(', '.join(str(i) for i in l) + '\n')
+
+
+        # output_file.write(key + '\t' + str(segment[0]) + '\t' + str(segment[1]) + '\t' + segment_sequence + '\t' + str(len(segment_sequence)) + '\t' + str(Gly) + '\t' + str(Ala) + '\t' +  str(Cys) + '\t' + str(Trp) + '\t' + str(Tyr) + '\t' + str(Pro) + '\t' + str(Thr) + '\t' + str(Ser) + '\t' + str(Asn) + '\t' + str(Gln) + '\t' + str(Asp) + '\t' + str(Glu) + '\t' + str(His) + '\t' + str(Lys) + '\t' + str(Arg) + '\t' + str(Met) + '\t' + str(Phe) + '\t' + str(Leu) + '\t' + str(Val) + '\t' + str(Ile) + '\n')
+
+
+
+        # output_file.write(key + '\t' + str(segment[0]) + '\t' + str(segment[1]) + '\t' + segment_sequence + '\t' + str(len(segment_sequence)) + '\t' + 'Gly' + '\t' + str(Gly) + '\t' + 'Ala' + '\t' + str(Ala) + '\t' + 'Cys' + '\t' + str(Cys) + '\t' + 'Trp' + '\t' + str(Trp) + '\t' + 'Tyr' + '\t' + str(Tyr) + '\t' + 'Pro' + '\t' + str(Pro) + '\t' + 'Thr' + '\t' + str(Thr) + '\t' + 'Ser' + '\t' + str(Ser) + '\t' + 'Asn' + '\t' + str(Asn) + '\t' + 'Gln' + '\t' + str(Gln) + '\t' + 'Asp' + '\t' + str(Asp) + '\t' + 'Glu' + '\t' + str(Glu) + '\t' + 'His' + '\t' + str(His) + '\t' + 'Lys' + '\t' + str(Lys) + '\t' + 'Arg' + '\t' + str(Arg) + '\t' + 'Met' + '\t' + str(Met) + '\t' + 'Phe' + '\t' + str(Phe) + '\t' + 'Leu' + '\t' + str(Leu) + '\t' + 'Val' + '\t' + str(Val) + '\t' + 'Ile' + '\t' + str(Ile) + '\n')
 
 sequence_file.close()
 output_file.close()
